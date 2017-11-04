d0e8db7fcba52f924415a3d75c4d920ca58e74e2
Yukun Ding <yding5@nd.edu>
2017-11-04 10:59:54 -0700

    image whited to standardization

--- cifar10_input.py
+++ cifar10_input.py
@@ -234,7 +234,7 @@ def inputs(eval_data, data_dir, batch_size):
                                                          width, height)
 
   # Subtract off the mean and divide by the variance of the pixels.
-  float_image = tf.image.per_image_whitening(resized_image)
+  float_image = tf.image.per_image_standardization(resized_image)
 
   # Ensure that the random shuffling has good mixing properties.
   min_fraction_of_examples_in_queue = 0.4
