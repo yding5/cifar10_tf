fix error computation of cifar10_error.py

cifar10_error.py
@@ -239,14 +239,16 @@ def evaluate():
     # Calcuate the error Y.D.
     print(logits.get_shape().as_list())
 
-    error_op = tf.subtract(logits,tf.to_float(labels))
+    label_matrix = one_hot(labels,10)
+
+    error_op = tf.subtract(logits,tf.to_float(label_matrix))
     error_max_op = tf.reduce_max(error_op,axis=1)
     error_sum_op = tf.reduce_sum(error_op)
     print(error_max_op.get_shape().as_list())
     print(error_sum_op.get_shape().as_list())
 
     softmax_logits = tf.nn.softmax (logits)
-    sm_error_op = tf.subtract(softmax_logits,tf.to_float(labels))
+    sm_error_op = tf.subtract(softmax_logits,tf.to_float(label_matrix))
     sm_error_max_op = tf.reduce_max(sm_error_op,axis=1)
     sm_error_sum_op = tf.reduce_sum(sm_error_op)
     print(sm_error_max_op.get_shape().as_list())